3c81baa10aa60b218a3995203bd025e1f1e48564
Kiëd Llaentenn <kiedtl@tilde.team>
2021-04-17 22:31:30 +0000

    Add WIP code for new parser module

--- src/parser.rs
+++ src/parser.rs
@@ -7,6 +7,280 @@ use pest::iterators::{Pair, Pairs};
 #[grammar = "grammar.pest"]
 pub struct ZfParser;
 
+#[cfg(test)]
+mod parser_tests {
+    use super::*;
+    use super::Node::*;
+
+    #[test]
+    fn test_basic() {
+        let s = "1 2 3 \"test\" abcd 99 'b'";
+        assert_eq!(do_parse(s).unwrap(), vec![
+            Number(1.), Number(2.), Number(3.), Str("test".to_owned()),
+            Word("abcd".to_owned()), Number(99.), Char(98.)
+        ]);
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum CondArm { Is(Vec<Node>), Any }
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum Node {
+    Decl((String, Vec<Node>)),
+    Quote(Vec<Node>),
+    Number(f64),
+    Str(String),
+    Char(f64),
+    Stack(String),
+    Fetch(String),
+    Store(String),
+    Word(String),
+    Refer(String),
+    Table(Vec<(Node, Node)>),
+    Guard((Vec<GuardItem>, Vec<GuardItem>)),
+    If((Vec<Node>, Option<Vec<Node>>)),
+    StackBlock((String, Vec<Node>)),
+    Until(Vec<Node>),
+    Loop(Vec<Node>),
+    Cond(Vec<(CondArm, Vec<Node>)>),
+    Ident(String),
+    Continue,
+    Break,
+}
+
+pub fn do_parse(source: &str) -> Result<Vec<Node>, Error<Rule>> {
+    let mut ast = Vec::new();
+    let pairs = ZfParser::parse(Rule::program, source)?;
+
+    for pair in pairs {
+        if let Some(node) = parse_node(pair) {
+            ast.push(node);
+        }
+    }
+
+    Ok(ast)
+}
+
+fn parse_node(pair: Pair<Rule>) -> Option<Node> {
+    match pair.as_rule() {
+        Rule::EOI => None,
+        Rule::word_decl => {
+            let mut items = pair.into_inner();
+    
+            let name;
+            if let Some(Node::Ident(s)) = parse_node(items.nth(0).unwrap()) {
+                name = s;
+            } else {
+                unreachable!();
+            }
+    
+            let body = items.nth(0).unwrap().into_inner()
+                .map(|p| parse_node(p))
+                .filter(|p| p.is_some()).map(|p| p.unwrap())
+                .collect::<Vec<Node>>();
+    
+            Some(Node::Decl((name, body)))
+        },
+        Rule::quote => {
+            let quote = pair.into_inner()
+                .map(|p| parse_node(p))
+                .filter(|p| p.is_some()).map(|p| p.unwrap())
+                .collect::<Vec<Node>>();
+    
+            if quote.len() == 1 {
+                if let Node::Word(w) = &quote[0] {
+                    return Some(Node::Refer(w.clone()));
+                }
+            }
+    
+            Some(Node::Quote(quote))
+        },
+        Rule::float => {
+            // FIXME: Rust's parse::<f64> doesn't support the following formats:
+            //  - 0__.0__e+10__, 0x2351, 0o261, 0b100111, 100_000_000
+            // At some point, a custom float-parsing function should be made.
+            //
+            // XXX: we trim whitespace because the grammar requires whitespace
+            // to be at the end of the literal.
+            let dstr = pair.as_str().trim_end();
+            let (sign, dstr) = match &dstr[..1] {
+                "_" => (-1.0, &dstr[1..]),
+                _ => (1.0, &dstr[..]),
+            };
+            let mut flt: f64 = match dstr.parse() {
+                Ok(fl) => fl,
+                Err(_) => panic!("`{}' is not a valid float", dstr),
+            };
+            if flt != 0.0 {
+                // Avoid negative zeroes; only multiply sign by nonzeroes.
+                flt *= sign;
+            }
+            Some(Node::Number(flt))
+        },
+        Rule::string => {
+            let str = &pair.as_str();
+            // Strip leading and ending quotes.
+            Some(Node::Str(str[1..str.len()-1].to_owned()))
+        },
+        Rule::character => {
+            let ch = &pair.as_str();
+            let ch = &ch[1..].chars().next().unwrap();
+            Some(Node::Char(*ch as u32 as f64))
+        },
+        Rule::word  => Some(Node::Word(pair.as_str().to_owned())),
+        Rule::stack => Some(Node::Stack(pair.as_str()[1..].to_owned())),
+        Rule::fetch => Some(Node::Fetch(pair.as_str()[1..].to_owned())),
+        Rule::store => Some(Node::Store(pair.as_str()[1..].to_owned())),
+        Rule::ident => Some(Node::Ident(pair.as_str().to_owned())),
+        Rule::table => {
+            let mut res = Vec::new();
+            let mut ctr = 0.;
+    
+            pair.into_inner().for_each(|item| {
+                match item.as_rule() {
+                    Rule::table_val => {
+                        let val = parse_node(item.into_inner().next().unwrap()).unwrap();
+                        res.push((Node::Number(ctr), val));
+                        ctr += 1.;
+                    },
+                    Rule::table_keyval => {
+                        let mut items = item.into_inner();
+                        let key = parse_node(items.nth(0).unwrap()).unwrap();
+                        let val = parse_node(items.nth(1).unwrap()).unwrap();
+                        res.push((key, val));
+                    },
+                    _ => unreachable!(),
+                }
+            });
+    
+            Some(Node::Table(res))
+        },
+        Rule::guard => {
+            let mut inner = pair.into_inner();
+            assert!(inner.clone().count() == 2);
+    
+            let mut guardsets = vec![];
+            for _ in 0..=1 {
+                let mut guardset = vec![];
+                let innerset = inner.next().unwrap().into_inner();
+                for minion in innerset {
+                    guardset.push(match minion.as_str() {
+                        "a" => GuardItem::Any,
+                        "n" => GuardItem::Number,
+                        "s" => GuardItem::Str,
+                        "q" => GuardItem::Quote,
+                        "*" => GuardItem::Unchecked,
+                        _   => panic!("'{}' is not a valid guard item",
+                            minion.as_str()),
+                    });
+                }
+                guardsets.push(guardset);
+            }
+    
+            Some(Node::Guard((guardsets[0].clone(), guardsets[1].clone())))
+        },
+        Rule::ifblk => {
+            let ifstmt;
+            let mut elsestmt = None;
+    
+            let mut ast = pair.into_inner();
+            ifstmt = ast.nth(0).unwrap().into_inner()
+                .map(|p| parse_node(p))
+                .filter(|p| p.is_some()).map(|p| p.unwrap())
+                .collect::<Vec<Node>>();
+    
+            let orelse_ast = ast.nth(0);
+            if orelse_ast.is_some() {
+                let elseblk = orelse_ast.unwrap().into_inner()
+                    .map(|p| parse_node(p))
+                    .filter(|p| p.is_some()).map(|p| p.unwrap())
+                    .collect::<Vec<Node>>();
+                elsestmt = Some(elseblk);
+            }
+    
+            Some(Node::If((ifstmt, elsestmt)))
+        },
+        Rule::stackblk => {
+            let mut ast = pair.into_inner();
+    
+            let name;
+            if let Some(Node::Ident(s)) = parse_node(ast.nth(0).unwrap()) {
+                name = s;
+            } else {
+                unreachable!();
+            }
+    
+            let stackblk = ast.nth(0).unwrap().into_inner()
+                .map(|p| parse_node(p))
+                .filter(|p| p.is_some()).map(|p| p.unwrap())
+                .collect::<Vec<Node>>();
+    
+            Some(Node::StackBlock((name, stackblk)))
+        },
+        Rule::continuestmt => Some(Node::Continue),
+        Rule::breakstmt    => Some(Node::Break),
+        Rule::loopblk => {
+            let loopblk = pair.into_inner()
+                .nth(0).unwrap().into_inner()
+                .map(|p| parse_node(p))
+                .filter(|p| p.is_some()).map(|p| p.unwrap())
+                .collect::<Vec<Node>>();
+            Some(Node::Loop(loopblk))
+        },
+        Rule::until => {
+            let loopblk = pair.into_inner()
+                .nth(0).unwrap().into_inner()
+                .map(|p| parse_node(p))
+                .filter(|p| p.is_some()).map(|p| p.unwrap())
+                .collect::<Vec<Node>>();
+            Some(Node::Until(loopblk))
+        },
+        Rule::cond => {
+            let mut arms = vec![];
+            let mut any  = None;
+
+            for arm_ast in pair.into_inner() {
+                if any.is_some() {
+                    // It's a syntax error for arms to come after the wildcard.
+                    panic!("Found match arm after wildcard");
+                }
+
+                match arm_ast.as_rule() {
+                    Rule::condarm => {
+                        let mut inner = arm_ast.into_inner();
+                        let cond = inner.nth(0).unwrap().into_inner()
+                            .map(|p| parse_node(p))
+                            .filter(|p| p.is_some()).map(|p| p.unwrap())
+                            .collect::<Vec<Node>>();
+                        let body = inner.nth(0).unwrap().into_inner()
+                            .map(|p| parse_node(p))
+                            .filter(|p| p.is_some()).map(|p| p.unwrap())
+                            .collect::<Vec<Node>>();
+                        arms.push((CondArm::Is(cond), body));
+                    },
+                    Rule::condany => {
+                        let body = arm_ast.into_inner()
+                            .nth(0).unwrap().into_inner()
+                            .map(|p| parse_node(p))
+                            .filter(|p| p.is_some()).map(|p| p.unwrap())
+                            .collect::<Vec<Node>>();
+                        any = Some((CondArm::Any, body));
+                    },
+                    _ => unreachable!(),
+                }
+            }
+
+            if any.is_some() {
+                arms.push(any.unwrap());
+            }
+
+            Some(Node::Cond(arms))
+        },
+        what_the_hell => panic!("Unexpected expression: {:?}", what_the_hell),
+    }
+}
+
 pub fn parse(env: &mut ZfEnv, source: &str)
     -> Result<Vec<ZfToken>, Error<Rule>>
 {
