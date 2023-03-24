c3ffd9a138e96d84ca9e830e38f30146ee78c5d0
Kiëd Llaentenn <kiedtl@tilde.team>
2023-03-23 21:32:16 -0400

    feat: make new actual
    What kind of commit message was that

--- src/utils.rs
+++
@@ -1,9 +0,0 @@
-macro_rules! pop_as {
-    ($e:ident, $t:ident) => {{
-        match $e.pop()? {
-            Value::$t(v) => v,
-            x => return Err(format!("expected {}, got {:?}",
-                    stringify!($t), x)),
-        }
-    }}
-}
